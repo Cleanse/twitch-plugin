2accddd0dde54768d89a8ebe530ec453a22a053e
Paul Lovato <plovato@gmail.com>
2017-08-06 05:54:40 -0600

    Config added.

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * You need a Client ID to access the Twitch API.
+ * Go here: https://www.twitch.tv/settings/connections
+ * - Scroll down until you see 'Developer Applications'
+ * - Register your application and enter your client id below.
+ */
+return [
+    'twitchId' => ''
+];
